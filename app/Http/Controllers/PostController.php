877354aeb5b2399127854f844bf18ed0719f46f8
hehexia <minus1s@qq.com>
2019-06-14 07:16:28 +0800

    路由模式和4个基础模版

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('post/index');
+    }
+
+    //
+    public function show()
+    {
+        return view('post/show');
+    }
+
+    //
+    public function create()
+    {
+        return view('post/create');
+    }
+
+    //编辑
+    public function store()
+    {
+    }
+
+    //编辑逻辑
+    public function update()
+    {
+    }
+
+    //删除逻辑
+    public function edit()
+    {
+        return view('post/edit');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return view('test');
+});
+//文章列表页
+Route::get('/posts', '\App\Http\Controllers\PostController@index');
+//创建文章
+Route::get('/posts/create', '\App\Http\Controllers\PostController@create');
+Route::post('/posts', '\App\Http\Controllers\PostController@store');
+//文章详情页
+Route::get('/posts/{post}', '\App\Http\Controllers\PostController@show');
+//编辑文章
+Route::get('/posts/{post}/edit', '\App\Http\Controllers\PostController@edit');
+Route::put('/posts/{post}', '\App\Http\Controllers\PostController@update');
+//删除
+Route::get('/posts/delete', '\App\Http\Controllers\PostController@delete');
